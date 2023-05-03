7952abfbb7917728d8132daacff65e21aad2f1cf
Allipson-ISO2 <34636@DESKTOP-DUIQ270>
2023-05-03 18:19:12 +0200

    implementacion codigo Centro.java

--- CodeLabs_ISO2/src/negocio/entities/Centro.java
+++ CodeLabs_ISO2/src/negocio/entities/Centro.java
@@ -9,6 +9,31 @@ public class Centro {
 	private String nombre;
 	private String localizacion;
 	private int attribute;
+	
+	public Centro(Collection<CursoPropio> cursoPropios, Collection<ProfesorUCLM> plantilla, String nombre,
+			String localizacion, int attribute) {
+		
+	}
+	
+	public Centro(String nombre, String localizacion, int attribute) {
+		
+	}
+	
 
+	public Collection<CursoPropio> getCursoPropios(){
+		return cursoPropios;
+	}
+	
+	public void setCursoPropios(Collection<CursoPropio> cursoPropios) {
+		
+	}
+	
+	public Collection<ProfesorUCLM> getPlantilla(){
+		return plantilla;
+	}
+	
+	public void setPlantilla(Collection<ProfesorUCLM> plantilla) {
+		
+	}
 	
 }
