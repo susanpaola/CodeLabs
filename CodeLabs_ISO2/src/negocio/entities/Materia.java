bc5324354663096166cc6f47462967df0b9cc1b3
Allipson-ISO2 <34636@DESKTOP-DUIQ270>
2023-05-04 17:56:13 +0200

    implementacion Materia.java

--- CodeLabs_ISO2/src/negocio/entities/Materia.java
+++ CodeLabs_ISO2/src/negocio/entities/Materia.java
@@ -7,7 +7,39 @@ public class Materia {
 	Profesor responsable;
 	private String nombre;
 	private double horas;
-	private Date fechaInicio;
-	private Date fechaFin;
+	private String fechaInicio;
+	private String fechaFin;
+	
+	public Profesor getResponsable() {
+		return responsable;
+	}
+	public void setResponsable(Profesor responsable) {
+		this.responsable = responsable;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getHoras() {
+		return horas;
+	}
+	public void setHoras(double horas) {
+		this.horas = horas;
+	}
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(String fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public String getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(String fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	
 
 }
