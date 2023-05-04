7f360d41cba742fd53cd9302add94b34e3ac9f83
Allipson-ISO2 <34636@DESKTOP-DUIQ270>
2023-05-04 18:25:27 +0200

    Getters y Setters clase Matricula

--- CodeLabs_ISO2/src/negocio/entities/Matricula.java
+++ CodeLabs_ISO2/src/negocio/entities/Matricula.java
@@ -1,14 +1,61 @@
 package negocio.entities;
 
-import java.util.Date;
+//import java.util.Date;
 
 public class Matricula {
 
-	Estudiante estudiante;
-	CursoPropio titulo;
-	ModoPago tipoPago;
-	private Date fecha;
+	private int idMatricula;
+	private int idTitulo;
+	private String idEstudiante;
+	private static ModoPago tipoPago;
+	private String fecha;
 	private boolean pagado;
-	private int attribute;
+	private int attributo;
+	
+	
+	public int getIdMatricula() {
+		return idMatricula;
+	}
+	public void setIdMatricula(int idMatricula) {
+		this.idMatricula = idMatricula;
+	}
+	public int getIdTitulo() {
+		return idTitulo;
+	}
+	public void setIdTitulo(int idTitulo) {
+		this.idTitulo = idTitulo;
+	}
+	public String getIdEstudiante() {
+		return idEstudiante;
+	}
+	public void setIdEstudiante(String idEstudiante) {
+		this.idEstudiante = idEstudiante;
+	}
+	public static ModoPago getTipoPago() {
+		return tipoPago;
+	}
+	public static void setTipoPago(ModoPago tipoPago) {
+		Matricula.tipoPago = tipoPago;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public boolean isPagado() {
+		return pagado;
+	}
+	public void setPagado(boolean pagado) {
+		this.pagado = pagado;
+	}
+	public int getAttributo() {
+		return attributo;
+	}
+	public void setAttributo(int attributo) {
+		this.attributo = attributo;
+	}
+
+	
 
 }
