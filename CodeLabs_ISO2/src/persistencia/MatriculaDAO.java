b499843b9163061c0cd428800110239b99494a52
susan <susanpaola.herrera@alu.uclm.es>
2023-05-05 18:21:34 +0200

    Crear metodo constructor

--- CodeLabs_ISO2/src/persistencia/MatriculaDAO.java
+++ CodeLabs_ISO2/src/persistencia/MatriculaDAO.java
@@ -1,5 +1,14 @@
 package persistencia;
 
 public class MatriculaDAO {
+	private GestorBD agenteBD;
+
+	public MatriculaDAO() {
+		try {
+			this.agenteBD = this.agenteBD.getAgente();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
 }
