bbf0ecd6d116d38579b99162b861a043ec094356
Allipson-ISO2 <34636@DESKTOP-DUIQ270>
2023-05-07 23:11:00 +0200

    Getters y Setters entidad

--- CodeLabs_ISO2/src/negocio/entities/ProfesorExterno.java
+++ CodeLabs_ISO2/src/negocio/entities/ProfesorExterno.java
@@ -4,6 +4,14 @@ public class ProfesorExterno extends Profesor {
 	
 	private String titulacion;
 
+	public String getTitulacion() {
+		return titulacion;
+	}
+
+	public void setTitulacion(String titulacion) {
+		this.titulacion = titulacion;
+	}
+
 	public ProfesorExterno(String dni, String nombre, String apellidos, boolean doctor, String titulacion) {
 		super(dni, nombre, apellidos, doctor);
 		this.titulacion = titulacion;
