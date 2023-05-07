718041f19fce4cf6443b231fbc7755cc7e844894
Allipson-ISO2 <34636@DESKTOP-DUIQ270>
2023-05-07 23:09:57 +0200

    Solucion de fallo en ProfesorExteno

--- CodeLabs_ISO2/src/negocio/entities/ProfesorExterno.java
+++ CodeLabs_ISO2/src/negocio/entities/ProfesorExterno.java
@@ -1,7 +1,15 @@
 package negocio.entities;
 
 public class ProfesorExterno extends Profesor {
-
+	
 	private String titulacion;
+
+	public ProfesorExterno(String dni, String nombre, String apellidos, boolean doctor, String titulacion) {
+		super(dni, nombre, apellidos, doctor);
+		this.titulacion = titulacion;
+		// TODO Auto-generated constructor stub
+	}
+
+	
 
 }
