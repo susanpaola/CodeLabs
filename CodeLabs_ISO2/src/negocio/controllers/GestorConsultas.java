a46fe977ce76eac799a423acbcb84e42c2888d05
Allipson-ISO2 <34636@DESKTOP-DUIQ270>
2023-05-08 11:15:03 +0200

    Gestor consultas creado con sus metodos correspondientes

--- CodeLabs_ISO2/src/negocio/controllers/GestorConsultas.java
+++ CodeLabs_ISO2/src/negocio/controllers/GestorConsultas.java
@@ -68,9 +68,8 @@ public class GestorConsultas {
 	 * @param fechaInicio
 	 * @param fechaFin
 	 */
-	public List<CursoPropio> consultarEstadoCursos(EstadoCurso estadoCurso, String fechaInicio, String fechaFin) {
-		// TODO - implement GestorConsultas.consultarEstadoCursos
-		throw new UnsupportedOperationException();String charsToRemove = "[] ";
+	public List<CursoPropio> consultarEstadoCursos(EstadoCurso estado, String fechaInicio, String fechaFin) {
+		String charsToRemove = "[] ";
 		List<CursoPropio> listaEstadoCurso = new ArrayList<>();
 		String sql = "SELECT * FROM CursoPropio WHERE estado='" + estado.toString() + "' AND fechaIni >='" + fechaInicio + "' AND fechaFin <='" + fechaFin + "'";
 		Vector<Object> vectorCursos = new Vector<Object>();
@@ -139,9 +138,118 @@ public class GestorConsultas {
 	 * @param fechaInicio
 	 * @param fechaFin
 	 */
-	public List<CursoPropio> listarEdicionesCursos(Date fechaInicio, Date fechaFin) {
-		// TODO - implement GestorConsultas.listarEdicionesCursos
-		throw new UnsupportedOperationException();
+	public List<CursoPropio> listarEdicionesCursos(String fechaInicio, String fechaFin) {
+		List<CursoPropio> listaEdicionesCurso = new ArrayList<>();
+		CursoPropioDAO cursoDAO = new CursoPropioDAO();
+		
+		String charsToRemove = "[] ";
+		String sql = "SELECT * FROM CursoPropio WHERE fechaIni >='" + fechaInicio + "' AND fechaFin <='" + fechaFin + "'";
+		Vector<Object> vectorCursos = new Vector<Object>();
+		try {
+			vectorCursos = cursoDAO.seleccionarCursos(sql);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		for (int i = 0; i < vectorCursos.size(); i++) {
+			String curso = vectorCursos.get(i).toString();
+				 
+		       	for (char c : charsToRemove.toCharArray()) {
+		        	curso = curso.replace(String.valueOf(c), "");
+			}
+		       
+		        String[] parts = curso.split(",");
+			
+			int id = Integer.parseInt(parts[0]);
+			String nombre = parts[1];
+			int ECTS = Integer.parseInt(parts[2]);
+			String fechaIni = parts[3];
+			String fechaFinParts = parts[4];
+			double tasa = Double.parseDouble(parts[5]);
+			int edicion = Integer.parseInt(parts[6]);
+			String centro = parts[7];
+			String director = parts[8];
+			String secretario = parts[9];
+			String estadoCurso = parts[10];
+			String tipoCurso = parts[11];
+			
+			EstadoCurso estado = null;
+			switch(estadoCurso) {
+				case "PROPUESTO":
+					estado = EstadoCurso.PROPUESTO;
+					break;
+				case "VALIDADO":
+					estado = EstadoCurso.VALIDADO;
+					break;
+				case "PROPUESTA_RECHAZADA":
+					estado = EstadoCurso.PROPUESTA_RECHAZADA;
+					break;
+				case "EN_MATRICULACION":
+					estado = EstadoCurso.EN_MATRICULACION;
+					break;
+				case "EN_IMPARTIZICION":
+					estado = EstadoCurso.EN_IMPARTIZICION;
+					break;
+				case "TERMINADO":
+					estado = EstadoCurso.TERMINADO;
+					break;
+			}
+			
+			TipoCurso tipo = null;
+			switch(tipoCurso) {
+				case "MASTER":
+					tipo = TipoCurso.MASTER;
+					break;
+				case "EXPERTO":
+					tipo = TipoCurso.EXPERTO;
+					break;
+				case "ESPECIALISTA":
+					tipo = TipoCurso.ESPECIALISTA;
+					break;
+				case "FORMACION_AVANZADA":
+					tipo = TipoCurso.FORMACION_AVANZADA;
+					break;
+				case "FORMACION_CONTINUA":
+					tipo = TipoCurso.FORMACION_CONTINUA;
+					break;
+				case "MICROCREDENCIALES":
+					tipo = TipoCurso.MICROCREDENCIALES;
+					break;
+				case "CORTA_DURACION":
+					tipo = TipoCurso.CORTA_DURACION;
+					break;
+			}
+			
+			CursoPropio cursoCreado = new CursoPropio(id, nombre, ECTS, fechaIni, fechaFinParts, tasa, edicion, centro, director, secretario, estado, tipo);
+			listaEdicionesCurso.add(cursoCreado);
+		}
+		
+		return listaEdicionesCurso;
 	}
+	
+	
+	public Vector<Object> estadoResuelto() throws Exception {
+		CursoPropioDAO cursoDAO = new CursoPropioDAO();
+		Vector<Object> a = cursoDAO.seleccionarCursos("SELECT id,nombre,centro,tipo,estado FROM CursoPropio WHERE `estado`<>'PROPUESTO'");
+		return a;
+	}
+	public Vector<Object> estadoPendiente() throws Exception {
+		CursoPropioDAO cursoDAO = new CursoPropioDAO();
+		Vector<Object> a = cursoDAO.seleccionarCursos("SELECT id,nombre,centro,tipo,estado FROM CursoPropio WHERE `estado`='PROPUESTO'");
+		return a;
+	}
+	
+	public Vector<Object> consultarCusos(String sql) throws Exception {
+		CursoPropioDAO cursoDAO = new CursoPropioDAO();
+		Vector<Object> a = cursoDAO.seleccionarCursos(sql);
+		return a;
+	}
+	
+	public Vector<Object> cursosDisponibles() throws Exception {
+		CursoPropioDAO cursoDAO = new CursoPropioDAO();
+		Vector<Object> a = cursoDAO.seleccionarCursos("SELECT id,nombre,centro,tipo,estado FROM CursoPropio WHERE `estado`='VALIDADO'");
+		return a;
+	}
+	
 
 }
