cb96ea4578a3b7d00f29af1fbb1d59cc95903997
Allipson-ISO2 <34636@DESKTOP-DUIQ270>
2023-05-08 12:11:40 +0200

    editarPropuestaCurso metodo completo

--- CodeLabs_ISO2/src/negocio/controllers/GestorPropuestasCursos.java
+++ CodeLabs_ISO2/src/negocio/controllers/GestorPropuestasCursos.java
@@ -21,9 +21,24 @@ public class GestorPropuestasCursos {
 	 * 
 	 * @param curso
 	 */
-	public void editarPropuestaCurso(CursoPropio curso) {
-		// TODO - implement GestorPropuestasCursos.editarPropuestaCurso
-		throw new UnsupportedOperationException();
+	public void editarPropuestaCurso(CursoPropio curso, int tipo) {
+		CursoPropioDAO agenteCursoPropioDAO = new CursoPropioDAO();
+		if (tipo == 0) { //edita todo
+			try {
+				String sql = "UPDATE CursoPropio SET nombre='" + curso.getNombre() + "', ECTS=" + curso.getECTS() + ", fechaIni='" + curso.getFechaInicio() + "', fechaFin='" + curso.getFechaFin() + "', tasa=" + curso.getTasaMatricula() + ", edicion=" + curso.getEdicion() + ", centro='" + curso.getCentro() + "', director='" + curso.getDirector() + "', secretario='" + curso.getSecretario() + "', estado='" + curso.getEstadoCurso().toString() + "', tipo='" + curso.getTipoCurso().toString() + "' WHERE id=" + curso.getId();
+				agenteCursoPropioDAO.editarCurso(sql);
+			
+			} catch (Exception e) {
+				System.out.println(e);
+			}  	
+		} else { //edita sólo el curso
+			try {
+				String sql = "UPDATE CursoPropio SET estado='" + curso.getEstadoCurso().toString() + "' WHERE id=" + curso.getId();
+				agenteCursoPropioDAO.editarCurso(sql);
+			} catch (Exception e) {
+				System.out.println(e);
+			} 
+		}
 	}
 
 	/**
