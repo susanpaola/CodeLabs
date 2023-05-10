4089d5c1701cda43d1c81090b3db0e456d5f6973
susanpaola <SusanPaola.Herrera@alu.uclm.es>
2023-05-10 08:26:57 +0200

    añadido el boton entrar

--- CodeLabs_ISO2/src/presentacion/PantallaConectar.java
+++ CodeLabs_ISO2/src/presentacion/PantallaConectar.java
@@ -1,7 +1,12 @@
 package presentacion;
 
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -34,8 +39,18 @@ public class PantallaConectar extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.WHITE);
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton conectBtn = new JButton("ENTRAR");
+		conectBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		conectBtn.setForeground(Color.WHITE);
+		conectBtn.setFont(new Font("Tahoma", Font.BOLD, 18));
+		conectBtn.setBackground(SystemColor.textHighlight);
+		conectBtn.setBounds(141, 172, 274, 113);
+		contentPane.add(conectBtn);
 	}
 
 }
