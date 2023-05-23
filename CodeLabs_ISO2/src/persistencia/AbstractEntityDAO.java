1444671f511c4f32d4772d394030b4c49b30e911
susanpaola <SusanPaola.Herrera@alu.uclm.es>
2023-05-23 10:29:26 +0200

    nuevas modificaciones

--- CodeLabs_ISO2/src/persistencia/AbstractEntityDAO.java
+++ CodeLabs_ISO2/src/persistencia/AbstractEntityDAO.java
@@ -1,10 +1,18 @@
 package persistencia;
 
+import java.util.Date;
+
 public abstract class AbstractEntityDAO<E> {
 	
 
+	private String id;
+	private Date fechaCreacion;
+	private Date fechaActualizacion;
+	private Class<E> entityClass;
+	
 	public AbstractEntityDAO(Class<E> entityClass) {
 		super();
+		this.entityClass = entityClass;
 	}
 
 	/**
