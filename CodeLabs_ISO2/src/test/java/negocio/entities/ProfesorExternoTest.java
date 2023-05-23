601cf9ec6b50e03b2b22309068619c057f79bcf5
susanpaola <SusanPaola.Herrera@alu.uclm.es>
2023-05-23 10:52:21 +0200

    realización de testing clase Profesor externo

---
+++ CodeLabs_ISO2/src/test/java/negocio/entities/ProfesorExternoTest.java
@@ -0,0 +1,36 @@
+package negocio.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProfesorExternoTest {
+
+    @Test
+    public void testGetTitulacion() {
+      
+        ProfesorExterno profesorExterno = new ProfesorExterno("123456789", "Juan", "Pérez", true, "Universidad");
+        String titulacion = profesorExterno.getTitulacion();
+        Assert.assertEquals("Universidad", titulacion);
+    }
+
+    @Test
+    public void testSetTitulacion() {
+        
+        ProfesorExterno profesorExterno = new ProfesorExterno("987654321", "María", "López", false, "");
+        profesorExterno.setTitulacion("M.Sc");
+        Assert.assertEquals("M.Sc", profesorExterno.getTitulacion());
+    }
+    
+    //CP Conjeura de error
+    @Test(expected = NullPointerException.class)
+    public void testConjeturaError() {
+   
+        ProfesorExterno profesorExterno = new ProfesorExterno("123456789", "Juan", "Pérez", true, null);
+        String titulacion = profesorExterno.getTitulacion();
+
+        
+    }
+}
